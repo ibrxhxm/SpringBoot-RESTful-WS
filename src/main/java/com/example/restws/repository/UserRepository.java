7c099e27d92e1fdc1f29f2af9e7ec8e49845ec83
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-10 13:56:12 +0100

    feat: create UserRepository class

---
+++ src/main/java/com/example/restws/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.restws.repository;
+
+import com.example.restws.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
