b9d687a7ff20f7a5da4eebd2212ed6859869c7e9
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-10 11:02:24 +0100

    refactor: edit createUser method

--- src/main/java/com/example/restws/controller/UserController.java
+++ src/main/java/com/example/restws/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.restws.controller;
 
+import com.example.restws.request.UserRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> createUser() {
+    public ResponseEntity<Object> createUser(@RequestBody UserRequest userRequest) {
         return null;
     }
 }
