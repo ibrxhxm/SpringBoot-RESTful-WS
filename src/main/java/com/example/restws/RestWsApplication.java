668d96ab98cf7c27799b828f02bdad56e19ea62b
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-10 10:17:06 +0100

    initial commit

---
+++ src/main/java/com/example/restws/RestWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWsApplication.class, args);
+	}
+
+}
