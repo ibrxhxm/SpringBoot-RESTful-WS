9d8b8153666b0bbae904d4c3af6c6f92673c9866
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-10 10:58:03 +0100

    refactor: update UserController

--- src/main/java/com/example/restws/controller/UserController.java
+++ src/main/java/com/example/restws/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.restws.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> createUser() {
         return null;
     }
