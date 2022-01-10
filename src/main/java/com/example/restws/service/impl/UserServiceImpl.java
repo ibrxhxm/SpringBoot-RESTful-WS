b151df122845b1c00e89934db22f6ed4d09c3f06
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-10 14:49:28 +0100

    refactor: update creatUser method in UserServiceImpl class

--- src/main/java/com/example/restws/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/restws/service/impl/UserServiceImpl.java
@@ -1,13 +1,31 @@
 package com.example.restws.service.impl;
 
 import com.example.restws.dto.UserDto;
+import com.example.restws.entity.UserEntity;
+import com.example.restws.repository.UserRepository;
 import com.example.restws.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+        userDto.setEncryptedPassword("test");
+        userDto.setUserId("testUserId");
+
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity = userRepository.save(userEntity);
+
+        return modelMapper.map(userEntity, UserDto.class);
     }
 }
