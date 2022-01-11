43b27f6069eaf459ed88651ed8d0ae646ca7db53
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-11 10:41:54 +0100

    feat: create SecurityConstants class

---
+++ src/main/java/com/example/restws/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.restws.constants;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 864000000;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGNUP_URL = "/users";
+    public static final String TOKEN_SECRET = "js875mwhshd9dy63";
+}
