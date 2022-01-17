bf5a80b3ead06b7bbae5a6a4c77b55afa4c437d8
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-17 16:53:45 +0100

    chore: add cofig beans to enable reading from .properties file

--- src/main/java/com/example/restws/RestWsApplication.java
+++ src/main/java/com/example/restws/RestWsApplication.java
@@ -2,6 +2,10 @@ package com.example.restws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class RestWsApplication {
@@ -10,4 +14,18 @@ public class RestWsApplication {
 		SpringApplication.run(RestWsApplication.class, args);
 	}
 
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
 }
