853b09ac4815d9f2afb48d884b940bc1ebeef4e2
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2022-01-21 11:38:13 +0100

    chore: add Address request model

---
+++ src/main/java/com/example/restws/request/AddressRequest.java
@@ -0,0 +1,40 @@
+package com.example.restws.request;
+
+public class AddressRequest {
+    private String street;
+    private String city;
+    private String country;
+    private String type;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
